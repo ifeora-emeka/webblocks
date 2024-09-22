69960a4885ba9bb4e8a4dafca61e4be068ed8a0b
Emeka Ifeora <34845973+Emyboy@users.noreply.github.com>
2024-09-22 16:22:22 +0100

    update

---
+++ apps/designer/src/app/dashboard/workspace/[workspace_id]/project/[project_id]/admin/components/AdminNav.tsx
@@ -0,0 +1,24 @@
+import EachDashboardNavLink from '@/components/layout/EachDashboardNavLink'
+import Link from 'next/link'
+import React from 'react'
+import { TbChartPie, TbMoneybag, TbSettings2 } from 'react-icons/tb'
+
+type Props = {}
+
+export default function AdminNav({ }: Props) {
+    return (
+        <>
+            <div className="p-default_spacing flex flex-col gap-default_spacing">
+                <Link href="">
+                    <EachDashboardNavLink Icon={TbChartPie} label="Overview" isActive />
+                </Link>
+                <Link href="">
+                    <EachDashboardNavLink Icon={TbMoneybag} label="Billing" />
+                </Link>
+                <Link href="">
+                    <EachDashboardNavLink Icon={TbSettings2} label="Settings" />
+                </Link>
+            </div>
+        </>
+    )
+}
--- apps/designer/src/app/dashboard/workspace/[workspace_id]/project/[project_id]/admin/layout.tsx
+++ apps/designer/src/app/dashboard/workspace/[workspace_id]/project/[project_id]/admin/layout.tsx
@@ -1,10 +1,11 @@
 import DashboardLayout from '@/components/layout/DashboardLayout'
 import React from 'react'
+import AdminNav from './components/AdminNav'
 
 export default function layout({ children }: any) {
     return (
         <>
-            <DashboardLayout leftNav={null}>{children}</DashboardLayout>
+            <DashboardLayout leftNav={<AdminNav />}>{children}</DashboardLayout>
         </>
     )
 }
